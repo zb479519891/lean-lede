c7ed603adfb2d5aa00932d5948b3b7afaa257bb8
zb479519891 <zhoubin04@gmail.com>
2021-06-26 01:48:39 +0800

    Initial commit

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,8 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
