a9f8ecba80895611ffa4f07b9e4e4c07ba24ebf8
麥克奈特 <zhoubin04@gmail.com>
2021-07-15 23:17:41 +0800

    helloworld

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # Uncomment a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
